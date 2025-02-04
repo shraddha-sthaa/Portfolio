188f8ce554ed859bc66c85324c9d0d43c0111972
Mallikarjun Aitha <68541427+mallikarjun25@users.noreply.github.com>
2025-02-04 03:46:29 -0500

    Update 2_🏆_Skills.py

--- pages/2_🏆_Skills.py
+++ pages/2_🏆_Skills.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set page configuration
 st.set_page_config(
-    page_title="Mallikarjun Aitha - Data Scientist",
+    page_title="Mallikarjun - Skills",
     page_icon="🕸️",
     layout="wide",
     initial_sidebar_state="expanded"
